3fbd5ee1638ec9c0b8a2e0044ccb4c2a5b9eb696
Vikaram <vikramstuart23@gmail.com>
2023-05-01 17:10:20 +0530

    Polynomial multiplication in Java involves multiplying two polynomials and obtaining a new polynomial as the result. In Java, a polynomial can be represented as an array of coefficients, where the index of the array corresponds to the degree of the term, and the value at the index represents the coefficient of that term.
    The steps for polynomial multiplication in Java are as follows:
    
    Create two arrays to represent the coefficients of the two polynomials.
    
    Determine the degree of the resulting polynomial, which is the sum of the degrees of the two input polynomials.
    
    Create a new array to represent the coefficients of the resulting polynomial, with a length equal to the degree of the resulting polynomial plus one.
    
    Use nested loops to multiply the coefficients of the two input polynomials and add the resulting terms to the corresponding index of the resulting polynomial array.
    
    Return the resulting polynomial array.

--- src/Expression.java
+++ src/Expression.java
@@ -1,2 +1,69 @@
-package PACKAGE_NAME;public class Expression {
+import java.util.Scanner;
+
+public class Expression {
+    Term termsList;
+    Term temp;
+
+    public static class Term{
+        int coefficient;
+        int[] variable;
+        Term next;
+
+        public Term(int coefficient,int[] variable){
+            this.coefficient = coefficient;
+            this.variable = variable;
+            this.next = null;
+        }
+    }
+
+    public Expression(){
+        this.termsList = null;
+        this.temp = null;
+    }
+    public void insertTerm(int numberOfTerms){
+        Scanner input = new Scanner(System.in);
+        int coefficient,numberOfVar;
+
+        for(int i = 0;i<numberOfTerms;i++){
+            System.out.print("Enter the coefficient"+(i+1)+" : ");
+            coefficient = input.nextInt();
+            System.out.print("Enter the number of variables (0-3) : ");
+            numberOfVar = input.nextInt();
+            int[] variables = new int[3];
+            for(int j = 0; j <numberOfVar; j++){
+                System.out.print("Enter the variable"+(j+1)+" : ");
+                char var = input.next().charAt(0);
+                System.out.print("Enter the exponent"+(j+1)+" : ");
+                variables[var-'x'] = input.nextInt();
+            }
+            insert(new Term(coefficient,variables));
+        }
+    }
+
+    public void insert(Term currTerm){
+        if(termsList == null){
+            termsList = currTerm;
+            temp = termsList;
+        }else {
+            temp.next = currTerm;
+            temp = temp.next;
+        }
+    }
+
+    public void multiply(Expression expression1, Expression expression2) {
+        Term temp1 = expression1.termsList, temp2 = expression2.termsList;
+        while(temp1 != null){
+            temp2 = expression2.termsList;
+            while (temp2 != null){
+                int coeff = temp1.coefficient*temp2.coefficient;
+                int[] variables = new int[3];
+                for(int i =  0;i<3;i++){
+                    variables[i] = temp1.variable[i]+temp2.variable[i];
+                }
+                insert(new Term(coeff,variables));
+                temp2 = temp2.next;
+            }
+            temp1 = temp1.next;
+        }
+    }
 }
--- src/PolynomialExpression.java
+++ src/PolynomialExpression.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class PolynomialExpression {
+import java.util.Scanner;
+
+public class PolynomialExpression {
+    static int numberOfExpression;
+    static Expression[] expressions;
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of Expressions : ");
+        numberOfExpression = input.nextInt();
+        expressions = new Expression[numberOfExpression];
+        for(int i = 0;i < numberOfExpression;i++){
+            Expression expression = new Expression();
+            System.out.print("Enter the number of Terms of Expression"+(i+1)+" : ");
+            int terms = input.nextInt();
+            expression.insertTerm(terms);
+            expressions[i] = expression;
+        }
+
+        for(int i = 1;i < numberOfExpression;i++){
+
+            Expression expression = new Expression();
+            expression.multiply(expressions[i-1],expressions[i]);
+            expressions[i] = expression;
+            for(int j = i;j<numberOfExpression;j++){
+                printExpression(expressions[j]);
+            }
+            System.out.println();
+        }
+    }
+    private static void printExpression(Expression expression) {
+        System.out.print("(");
+        Expression.Term temp1 = expression.termsList;
+        while (temp1 != null){
+            System.out.print(temp1.coefficient<0?"-":"+");
+            System.out.print("("+Math.abs(temp1.coefficient));
+            for(int i = 0;i<3;i++){
+                if(temp1.variable[i]!=0) System.out.print((char)(i+'x')+"^"+temp1.variable[i]);
+            }
+            System.out.print(")");
+            temp1 = temp1.next;
+        }
+        System.out.print(") ");
+    }
 }
